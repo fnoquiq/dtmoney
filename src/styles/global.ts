605a431a2c9eeee2718abf33fcebf34ba2f87fc1
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-05-09 09:19:41 -0300

    :rocket: Estrutura do formulário

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -53,4 +53,22 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    background: rgba(0,0,0,0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .react-modal-content {
+
+  }
 `
